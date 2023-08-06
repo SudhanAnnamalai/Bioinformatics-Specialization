bd7bc366bb84f231ffbdb0c44a16b3f3940efc71
Sudharsan A <sudharshanvas@gmail.com>
2023-08-06 20:34:14 +0530

    Update DPChange.py
    Some comments added

--- DPChange.py
+++ DPChange.py
@@ -7,7 +7,9 @@ coi = sys.argv[2].split(",")
 coins = [int(i) for i in coi]
 
 def DPChange(money, Coins):
+        #Takes the amount - money, Coins - list of allowed coins
     MinNumCoins=[0 for i in range(money+1)]
+    #MinNumCoins - Empty placeholder at the size of amount
     for m in range(1,money):
         MinNumCoins[m]=float('inf')
         for i in range(0,len(Coins)-1):
@@ -18,3 +20,4 @@ def DPChange(money, Coins):
     return MinNumCoins[money-1]
     
 DPChange(money,coins)
+
